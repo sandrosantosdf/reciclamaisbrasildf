ecfaf2615934266e66282c4ae44b957898d2b671
sandrosantosdf <sandrosantosdf@gmail.com>
2022-08-31 13:34:00 -0300

    Criado

---
+++ src/main/java/com/rmbdf/ReciclamaisbrasildfApplication.java
@@ -0,0 +1,13 @@
+package com.rmbdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclamaisbrasildfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclamaisbrasildfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmbdf/ReciclamaisbrasildfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmbdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclamaisbrasildfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
